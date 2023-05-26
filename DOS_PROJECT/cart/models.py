e66875b12a46d1f30dc41e3a47f02bc54f465fa2
scarface1129 <agboemmanuel002@gmail.com>
2023-05-26 07:10:45 +0100

    created a new model orders and cart

--- DOS_PROJECT/cart/models.py
+++ DOS_PROJECT/cart/models.py
@@ -10,10 +10,28 @@ from django.db.models.signals import post_save
 # Create your models here.
 User = settings.AUTH_USER_MODEL
 
-class Order(models.Model):
+
+class Cart(models.Model):
+    paid = models.BooleanField(default = True)
     user_id = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.user_id.username
+
+def post_save_user_receiver(sender, instance, created, *args, **kwargs):
+	if created:
+		cart, is_created = Cart.objects.get_or_create(user_id=instance)
+		
+
+
+post_save.connect(post_save_user_receiver, sender=User)
+
+
+class Order(models.Model):
     course_id = models.ForeignKey(to=Courses, on_delete=models.CASCADE)
     cart_id = models.ForeignKey(to=Cart, on_delete=models.CASCADE)
 
-class Cart(models.Model):
-    paid = models.BooleanField(default = False)
+    def __str__(self):
+        return self.cart_id.user_id.Name
+    class Meta:
+        verbose_name_plural = "Order"
